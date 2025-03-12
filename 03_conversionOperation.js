234aa00f237939493c4fd76aebc2754b163eff8c
Kshitij1231 <pandeydish763@gmail.com>
2025-03-12 13:58:14 +0530

    conversion operation

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -22,10 +22,49 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+ 
+// operation
+let value =3
+let negValue =-value
+// console.log(negValue);
 
-let a=12;
-let b=13;
-let c=a+b;
-console.log(c);
+// let a=12;
+// let b=13;
+// let c=a+b;
+// console.log(c);
+
+// console.log(3+3);
+// console.log(3-3);
+// console.log(3**3);
+// console.log(3*4);
+// console.log(4/3);
+// console.log(2%4);
+
+let str1 ="hello"
+let str2 =" Kshitij"
+let str3 =str1 + str2
+// console.log(str3);
+// console.log("1" +3);
+// console.log(1+"3");
+// console.log("1"+2+3);
+// console.log(1+2+"3");
+// console.log(3+4*5%7);
+ 
+// console.log(+true);
+// // console.log(true+);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 =2 + 2
+
+let gameCounter =100
+++gameCounter;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
