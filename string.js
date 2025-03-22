5243ff827b609bdb5bc55c0856c56ca22d14e6cc
Kshitij1231 <pandeydish763@gmail.com>
2025-03-23 00:15:30 +0530

    string learn

---
+++ string.js
@@ -0,0 +1,34 @@
+const name = "Kshitij"
+const repoCount =40
+
+// console.log(name + repoCount + " values");
+ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+ 
+ const gameName = new String("Kshitij-pandey")
+//  console.log(gameName);
+//  console.log(gameName[1]);
+//  console.log(gameName.__proto__);
+
+
+//  console.log(gameName.length);
+//  console.log(gameName.toUpperCase());
+ console.log(gameName.charAt(4));
+ console.log(gameName.indexOf('y'));
+
+ const newString = gameName.substring(0,4)
+ console.log(newString);
+ 
+ const anotherString = gameName.slice(2,-8)
+ console.log(anotherString);
+ const newStringOne = "   Kshitij    "
+ console.log(newStringOne);
+ console.log(newStringOne.trim());
+ 
+ const url ="https://kshitij.com/web3%40cryptospace"
+ console.log(url);
+ console.log(url.replace('%40','-'));
+
+console.log(url.includes("kshitij"));
+ console.log(url.includes("blockchain"));
+  console.log(gameName.split('-'));
+  
